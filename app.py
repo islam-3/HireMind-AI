3335b19638dfc6f86b4485b65b1bd4f3713590b5
islam-3 <islamalhams@gmail.com>
2026-04-29 04:57:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if not st.session_state.entered:
         </div>
     """, unsafe_allow_html=True)
 
-    col1, col2, col3 = st.columns([2.5, 2, 2.5])
+    col1, col2, col3 = st.columns([3, 3, 3])
     with col2:
         if st.button("Access Professional Suite"):
             st.session_state.entered = True
